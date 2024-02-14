54ac7141221e323d766f30cc5e86ff99b055a94d
2497Vaishnavi <158637463+2497Vaishnavi@users.noreply.github.com>
2024-02-14 11:51:58 -0500

    create even_numbers.py

--- Integration/Login.py
+++
@@ -1,8 +0,0 @@
-# login.py
-
-def login(username, password):
-    # Dummy login functionality
-    if username == "user" and password == "password":
-        return True
-    else:
-        return False
---
+++ Integration/even_numbers.py
@@ -0,0 +1,5 @@
+# even_numbers.py
+
+def is_even(number):
+    return number % 2 == 0
+
