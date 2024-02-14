7efbf4e45672e2a60db085a98bd1baa780687f4f
2497Vaishnavi <158637463+2497Vaishnavi@users.noreply.github.com>
2024-02-14 15:15:16 -0500

    Update integration_test.py

--- Integration/integration_test.py
+++ Integration/integration_test.py
@@ -1,10 +1,7 @@
 # integration_test.py
-
 from even_numbers import is_even
-
 def test_even_number():
     assert is_even(4) == True
-
 def test_odd_number():
     assert is_even(3) == False
 from even_numbers import is_odd
