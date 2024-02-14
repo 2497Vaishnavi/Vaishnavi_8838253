75b4d293c5271f16798aaa042b9462bcb33c5607
2497Vaishnavi <158637463+2497Vaishnavi@users.noreply.github.com>
2024-02-14 10:52:45 -0500

    Create Login.py

---
+++ Integration/Login.py
@@ -0,0 +1,8 @@
+# login.py
+
+def login(username, password):
+    # Dummy login functionality
+    if username == "user" and password == "password":
+        return True
+    else:
+        return False
